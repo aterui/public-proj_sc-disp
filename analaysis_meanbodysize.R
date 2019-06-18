3d782e3b4058ce0a93542f9b83965c3912db84fe
AkiraTerui <hanabi0111@gmail.com>
2019-06-18 11:08:28 -0500

    1stSubmission

--- analaysis_meanbodysize.R
+++ analaysis_meanbodysize.R
@@ -2,5 +2,5 @@
 dat_raw <- read.csv("data/data_itg2019-03-29.csv")
 sp <- sort(unique(dat_raw$species))
 
-mu <- sapply(sp, function(x) plot(density(dat_raw[dat_raw$species==sp[x]&dat_raw$Yrecap==1, "St_Size"])) )
+mu <- sapply(sp, function(x) mean(dat_raw[dat_raw$species==sp[x]&dat_raw$Yrecap==1, "St_Size"]) )
 SD <- sapply(sp, function(x) sd(dat_raw[dat_raw$species==sp[x]&dat_raw$Yrecap==1, "St_Size"]) )
