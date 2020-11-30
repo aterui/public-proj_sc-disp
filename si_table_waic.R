e6e7d41016f976f1418011444285b7da9a9a72a8
AkiraTerui <hanabi0111@gmail.com>
2020-11-29 21:24:54 -0500

    Create si_table_waic.R

---
+++ si_table_waic.R
@@ -0,0 +1,36 @@
+
+# library -----------------------------------------------------------------
+
+  pacman::p_load(tidyverse, kableExtra)
+
+
+# read data ---------------------------------------------------------------
+
+  filenames <- list.files(path = here::here("bayes_waic"), full.names = TRUE)
+  species <- rep(unlist(lapply(filenames, function(x) c(na.omit(str_extract(x, pattern = c('BHC', 'CRC', 'STJ')))))),
+                 each = 3)
+  Q <- rep(unlist(lapply(filenames, function(x) c(na.omit(str_extract(x, pattern = c('q50', 'q99')))))),
+           each = 3)
+  
+  dat_tab <- lapply(filenames, read_csv) %>% 
+    bind_rows() %>% 
+    mutate(Species = species,
+           Flow = Q,
+           Estimate = round(Estimate, 1)) %>% 
+    filter(X1 == "waic") %>% 
+    mutate(Flow = case_when(Flow == 'q99' ~ "High flow model",
+                            Flow == 'q50' ~ "Median flow model",
+                            TRUE ~ as.character(Flow))) %>% 
+    select(Species,
+           Flow,
+           Estimate) %>% 
+    pivot_wider(id_cols = Species,
+                names_from = Flow,
+                values_from = Estimate) 
+    
+  tab <- dat_tab %>% 
+    kbl(format = "latex", booktabs = TRUE, longtable = TRUE) %>% 
+    add_header_above(c("", "WAIC" = 2)) %>% 
+    kable_styling(latex_options = c("repeat_header"))
+  
+  print(tab)
