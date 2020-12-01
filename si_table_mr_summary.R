228da6003a5fa93125e26060725c7ab212c30dd3
AkiraTerui <hanabi0111@gmail.com>
2020-11-30 22:04:17 -0500

    Update si_table_mr_summary.R

--- si_table_mr_summary.R
+++ si_table_mr_summary.R
@@ -9,22 +9,27 @@
 
   dat <- read_csv("data_fmt/vector_data.csv")
   
-  tab <- dat %>% 
+  dat_tab <- dat %>% 
     group_by(stream, species) %>% 
     summarise(n_unique_cap = n_distinct(tag_id),
               n_unique_recap = n_distinct(tag_id[!is.na(section_2)]),
               n_replicate_cap = n(),
               n_replicate_recap = sum(!is.na(section_2))) %>% 
     ungroup() %>% 
+    group_by(stream) %>% 
+    mutate(stream = c(unique(stream), rep("", n()-1)))
+  
+  tab <- dat_tab %>% 
     kbl(format = "latex",
-        booktabs = T,
-        escape = F,
+        booktabs = TRUE,
+        escape = FALSE,
         col.names = linebreak(c('Stream',
                                'Species',
                                'Capture\n(unique)',
                                'Recapture\n(unique)',
                                'Capture\n(total replicate)',
                                'Recapture\n(total replicate)'),
-                              align = "c"))
+                              align = "c")) %>% 
+    kable_styling(latex_options = "hold_position")
   
   print(tab)
