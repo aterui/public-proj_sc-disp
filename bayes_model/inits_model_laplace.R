1bd405b61265728d55da9ce54c91109e46da8f19
AkiraTerui <hanabi0111@gmail.com>
2021-04-07 15:43:25 -0400

    Update inits_model_laplace.R

--- bayes_model/inits_model_laplace.R
+++ bayes_model/inits_model_laplace.R
@@ -5,7 +5,7 @@
   pacman::p_load(tidyverse, runjags, loo)
 
 # read data ---------------------------------------------------------------
-  
+
   dat_raw <- read_csv("data_fmt/vector_data.csv")
   flow <- read.csv("data_fmt/flow_summary.csv")
   temp <- read.csv("data_fmt/temp_summary.csv")
@@ -13,21 +13,21 @@
   
   var_set <- expand.grid(species = unique(dat_raw$species),
                          flow = c("q50", "q99"))
-  
+
 # MCMC setup --------------------------------------------------------------
-  
+
   n.ad <- 100
   n.iter <- 8E+3
   n.thin <- max(3, ceiling(n.iter/500))
   burn <- ceiling(max(10, n.iter/2))
   Sample <- ceiling(n.iter/n.thin)
-  
-  
-# run jags ----------------------------------------------------------------
 
-for(i in 1:nrow(var_set)) {
+
+# run jags ----------------------------------------------------------------
+  
+  for(i in 1:nrow(var_set)) {
     
-  # data prep ---------------------------------------------------------------
+    # data prep ---------------------------------------------------------------
     
     species <- var_set$species[i]
     FQ <- var_set$flow[i]
@@ -51,7 +51,7 @@ for(i in 1:nrow(var_set)) {
              scl_q50 = c(scale(q50)),
              interval = julian_2 - julian_1,
              upper_limit = ifelse(stream == "Indian", 740, 520))
-  
+    
     ## row IDs with observations
     XID <- which(!is.na(dat$X))
     
@@ -67,7 +67,7 @@ for(i in 1:nrow(var_set)) {
       para <- c("b", "sigma", "mu.phi", "sigma.phi", "mu", "sigma", "loglik")
     } 
     
-  # data for JAGS -----------------------------------------------------------
+    # data for JAGS -----------------------------------------------------------
     
     data_jags <- list(Y = dat$Y,
                       X = dat$X,
@@ -90,12 +90,12 @@ for(i in 1:nrow(var_set)) {
                       X_length = seq(min(dat$scl_length, na.rm = T),
                                      max(dat$scl_length, na.rm = T),
                                      length = 100)
-                      )
+    )
     
     inits <- replicate(3,
                        list(b = c(3.5, rep(0.3, 5)), 
-                               .RNG.name = "base::Wichmann-Hill",
-                               .RNG.seed = NA),
+                            .RNG.name = "base::Wichmann-Hill",
+                            .RNG.seed = NA),
                        simplify = FALSE)
     
     for(i in 1:3) {
@@ -115,18 +115,18 @@ for(i in 1:nrow(var_set)) {
                      thin = n.thin,
                      n.sims = 3,
                      modules = "glm")
-  
+    
     result <- MCMCvis::MCMCsummary(post$mcmc)
-  
-  # waic --------------------------------------------------------------------
+    
+    # waic --------------------------------------------------------------------
     
     loglik <- sapply(XID,
                      function(i) unlist(post$mcmc[, paste0("loglik[", i, "]")]))
     WAIC <- waic(loglik)
-  
-  # save output -------------------------------------------------------------
+    
+    # save output -------------------------------------------------------------
     
     write.csv(result[!str_detect(rownames(result), "loglik"),], paste0("bayes_estimate/summary_", n.iter, species, FQ,".csv") )
     write.csv(WAIC$estimates, paste0("bayes_waic/waic_", n.iter, species, FQ,".csv") )
     
-}
+  }
